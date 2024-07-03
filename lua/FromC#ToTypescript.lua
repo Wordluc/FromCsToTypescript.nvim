6e77d811bb1f0247498df2df272c7b5250051a47
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-07-04 00:00:16 +0200

    feat: bozza client

--- lua/FromC#ToTypescript.lua
+++ lua/FromC#ToTypescript.lua
@@ -1,17 +1,34 @@
 local uv = require('luv')
-
 local M = {}
-M.create_server = function (host, port)
-	local server = uv.new_udp()
-	server:bind(host, port)
-	print("Listening on " .. host .. ":" .. port)
-	server:recv_start(function(err, data, from)
-		assert(not err, err)
-		if data then
-			print(data)
-		end
-	end)
-	uv.run()
-end
 
+M.create_server = function(host, port)
+	local r=os.execute("C:\\Users\\frang\\Desktop\\Training\\FromCsToTypescript\\nvimPlugin\\src\\GoFromCsToTypescript\\GoFromCsToTypescript.exe " .. port)
+	local client = uv.new_udp()
+	client:send("public class prova{string ccc;}", host, port, function(err)
+		if err then
+			print("Error sending data: ", err)
+			return
+		end
+		client:recv_start(function(err, data, addr, flags)
+			if err then
+				print("Error receiving data: ", err)
+				return
+			end
+			if data then
+				print("holla")
+				local prova = vim.json.decode(data)
+				if prova.Status.Code == 200 then
+					vim.schedule(function()
+						vim.fn.setreg("", prova.Data)
+					end)
+				else
+					print("Error")
+				end
+			end
+		end)
+	end)
+end
+uv.run() -- This is necessary to start the event loop
+
+--M.create_server("127.0.0.1", 90)
 return M
