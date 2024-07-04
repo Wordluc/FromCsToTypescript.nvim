f8b5e2bf24e2cfa8a2edebacd5bf6322a007d737
Luca.Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-07-04 10:48:29 +0200

    fix: client

--- lua/FromC#ToTypescript.lua
+++ lua/FromC#ToTypescript.lua
@@ -1,10 +1,9 @@
 local uv = require('luv')
 local M = {}
 
-M.create_server = function(host, port)
-	local r=os.execute("C:\\Users\\frang\\Desktop\\Training\\FromCsToTypescript\\nvimPlugin\\src\\GoFromCsToTypescript\\GoFromCsToTypescript.exe " .. port)
+local function manage_server(str, host, port)
 	local client = uv.new_udp()
-	client:send("public class prova{string ccc;}", host, port, function(err)
+	client:send(str, host, port, function(err)
 		if err then
 			print("Error sending data: ", err)
 			return
@@ -15,20 +14,32 @@ M.create_server = function(host, port)
 				return
 			end
 			if data then
-				print("holla")
 				local prova = vim.json.decode(data)
 				if prova.Status.Code == 200 then
 					vim.schedule(function()
-						vim.fn.setreg("", prova.Data)
+						vim.fn.setreg("", prova.Body)
 					end)
 				else
-					print("Error")
+					print("Error:" .. prova.Status.Msg)
 				end
 			end
 		end)
 	end)
 end
-uv.run() -- This is necessary to start the event loop
 
---M.create_server("127.0.0.1", 90)
+M.create_server = function(host, port, str)
+	local handle, server_process = uv.spawn(
+		"C://Users//Usernexus//Desktop//PersonalProject//FromCsToTypeScript_PluginNvim//src//GoFromCsToTypescript//GoFromCsToTypescript.exe "
+		, { args = { 90 } }, function() end
+	)
+	if server_process then
+		manage_server(str, host, port)
+	else
+		print("Error starting server creation job.")
+	end
+end
+
+uv.run() -- This is necessary to start the event loop
+local str = vim.fn.getreg("")
+M.create_server("127.0.0.1", 90, str)
 return M
