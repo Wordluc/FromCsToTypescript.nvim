a840be0f93600d50df6968ea66ea507e26ad736c
Luca.Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-07-04 20:15:48 +0200

    refactor: signature

--- lua/FromC#ToTypescript.lua
+++ lua/FromC#ToTypescript.lua
@@ -1,14 +1,14 @@
 local uv = require('luv')
 local M = {}
 
-local function manage_server(str, host, port)
+local function manage_server(reg, host, port)
 	local client = uv.new_tcp()
 	client:connect(host, port, function(err)
 		if err then
 			print("Error connecting to server:", err)
 			return
 		end
-		client:write(str)
+		client:write(vim.fn.getreg(reg))
 		client:read_start(function(err, data)
 			if err then
 				print("Error receiving data: ", err)
@@ -19,7 +19,7 @@ local function manage_server(str, host, port)
 				if response.Status.Code == 200 then
 					vim.schedule(function()
 						print("Converted")
-						vim.fn.setreg("", response.Body)
+						vim.fn.setreg(reg, response.Body)
 					end)
 				else
 					print("Error:", response.Status.Msg)
@@ -30,7 +30,10 @@ local function manage_server(str, host, port)
 	end)
 end
 
-M.convertDto = function(port, str)
+M.convertDto = function(port, reg)
+	if reg==nil then
+    reg=""
+	end
 	uv.run("nowait") -- This is necessary to start the event loop
 	local script_path = debug.getinfo(1, "S").source:sub(2)
 	local executable_path = script_path:match(".*/") .. "../src/GoFromCsToTypescript/GoFromCsToTypescript.exe"
@@ -43,9 +46,9 @@ M.convertDto = function(port, str)
 		print("Error starting server:" .. pid)
 	end
 	local timer = uv.new_timer()
-	timer:start(100, 0, function()
+	timer:start(200, 0, function()
 		if pid then
-			manage_server(str, "127.0.0.1", port)
+			manage_server(reg, "127.0.0.1", port)
 		else
 			print("Error starting server creation job.")
 		end
