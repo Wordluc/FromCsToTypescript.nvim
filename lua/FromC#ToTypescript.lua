ca53a8e92bde1a76ce99fa19384ebc21ab9e7e09
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-07-05 08:08:40 +0200

    refactor: comunicazione utilizzando pipe

--- lua/FromC#ToTypescript.lua
+++ lua/FromC#ToTypescript.lua
@@ -1,63 +1,38 @@
 local uv = require('luv')
 local M = {}
 
-local function manage_server(reg, host, port)
-	local client = uv.new_tcp()
-	client:connect(host, port, function(err)
-		if err then
-			print("Error connecting to server:", err)
-			return
-		end
-		vim.schedule(function()
-			client:write(vim.fn.getreg(reg))
-		end)
-		client:read_start(function(err, data)
-			if err then
-				print("Error receiving data: ", err)
-				return
-			end
-			if data then
-				local response = vim.json.decode(data)
-				if response.Status.Code == 200 then
-					vim.schedule(function()
-						print("Converted")
-						vim.fn.setreg(reg, response.Body)
-					end)
-				else
-					print("Error:", response.Status.Msg)
-				end
-				client:close()
-			end
-		end)
-	end)
-end
-
-M.convertDto = function(port, reg)
-	if reg == nil then
+M.convertDto = function(reg)
+	if not reg then
 		reg = ""
 	end
 	uv.run("nowait") -- This is necessary to start the event loop
-	local script_path = debug.getinfo(1, "S").source:sub(2)
-	local executable_path = script_path:match(".*/") .. "../src/GoFromCsToTypescript/GoFromCsToTypescript.exe"
-	--local executable_path = "..\\..\\src\\GoFromCsToTypescript\\GoFromCsToTypescript.exe"
-	local handle, pid = uv.spawn(
+	local stdin = uv.new_pipe()
+	local stdout = uv.new_pipe()
+	local stderr = uv.new_pipe()
+	local executable_path = "~\\..\\src\\GoFromCsToTypescript\\GoFromCsToTypescript.exe"
+
+	local _, pid = uv.spawn(
 		executable_path
-		, { args = { port } }, function() end
+		, { stdio = { stdin, stdout, stderr } }, function() end
 	)
-	if handle == nil then
-		print("Error starting server:" .. pid)
+	if not pid then
+		print("Error starting server")
+		return
 	end
-	local timer = uv.new_timer()
-	timer:start(200, 0, function()
-		if pid then
-			manage_server(reg, "127.0.0.1", port)
-		else
-			print("Error starting server creation job.")
+	stdin:write(vim.fn.getreg(reg))
+	stdout:read_start(function(_, data)
+		if data then
+			vim.schedule(function()
+				print("Converted")
+				vim.fn.setreg(reg)
+			end)
 		end
-
-		timer:close()
 	end)
-
+	stderr:read_start(function(_, data)
+		if data then
+			print(data)
+		end
+	end)
 	uv.stop()
 end
 return M
